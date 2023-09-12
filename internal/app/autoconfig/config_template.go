f126f1b35c5c1959fcd08f983e5bcb38126cb755
Jackson Barreto <jackson.barreto@gmail.com>
2023-09-12 12:05:14 +0200

    feat: nginx fragment template

---
+++ internal/app/autoconfig/config_template.go
@@ -0,0 +1,10 @@
+package autoconfig
+
+const configTemplate = `
+location /{{.Name}}/ {
+    allow 127.0.0.1;
+    deny all;
+    auth_request /auth-verify;
+    proxy_pass http://{{.Hostname}}:{{.Port}}/;
+}
+`
