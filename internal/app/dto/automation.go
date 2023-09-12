f5f13819dc0bbd5c4c3cd1b541876754a43e2a03
Jackson Barreto <jackson.barreto@gmail.com>
2023-09-12 13:41:26 +0200

    feat: validator

--- internal/app/dto/automation.go
+++ internal/app/dto/automation.go
@@ -1,7 +1,24 @@
 package dto
 
+import (
+	"fmt"
+)
+
 type Automation struct {
 	Name     string `json:"name"`
 	Hostname string `json:"hostname"`
 	Port     int    `json:"port"`
+}
+
+func (a *Automation) Validate() error {
+	if a.Name == "" {
+		return fmt.Errorf("name is required")
+	}
+	if a.Hostname == "" {
+		return fmt.Errorf("hostname is required")
+	}
+	if a.Port <= 0 || a.Port > 65535 {
+		return fmt.Errorf("error: Port %d is not valid", a.Port)
+	}
+	return nil
 }
