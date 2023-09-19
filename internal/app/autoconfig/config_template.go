daa80b5d8bc6dbdf3177d55d44212f1f79db9ddf
Jackson Barreto <jackson.barreto@gmail.com>
2023-09-19 14:07:17 +0200

    fix: template

--- internal/app/autoconfig/config_template.go
+++ internal/app/autoconfig/config_template.go
@@ -2,8 +2,6 @@ package autoconfig
 
 const configTemplate = `
 location /{{.Name}} {
-    allow 127.0.0.1;
-    deny all;
     auth_request /auth-verify;
     proxy_pass http://{{.Host}}:{{.Port}};
 }
